8205fed1202f544f1119ce387f87e6c48120e778
Ayhan Kırıkcı <ayhankirikci2264@gmail.com>
2022-06-11 02:55:01 +0300

    chat profile header added

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -54,6 +54,64 @@ export default function Chat() {
                 </svg>
               </div>
             </header>
+            <div className="flex w-full text-white divide-x divide-wp-white-200">
+              <div className="w-1/3">
+                <div className="flex items-center justify-between bg-wp-secondary-100">
+                  <div>
+                    <img
+                      className="w-10 m-2 rounded-full"
+                      alt="profile picture"
+                      src="https://via.placeholder.com/50"
+                    />
+                  </div>
+                  <div className="flex justify-around w-28 text-wp-white-200">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 4v16m8-8H4"
+                      />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div className="w-2/3">2</div>
+            </div>
           </div>
         </main>
       </div>
